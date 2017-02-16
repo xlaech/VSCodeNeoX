e1f97e31f6b547e37ba5db4578e60c5af728ccf3
Nicolas Antor <n.antor@outlook.com>
2017-02-16 09:57:04 +0900

    Collect letters until they are 'approved' by enter

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,10 @@
 'use strict';
+import { log } from 'util';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+
+var pressedKeysList: string[] = [];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +12,14 @@ export function activate(context: vscode.ExtensionContext) {
 
     function registerCommandNice(commandId: string, run: (...args: any[]) => void): void {
 		context.subscriptions.push(vscode.commands.registerCommand(commandId, run));
+	}
+
+    function type(text: string): void {	
+        console.log(text);
+
+        vscode.commands.executeCommand('default:type', {
+            text: text
+        });
 	}
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -20,7 +31,13 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 		
-        console.log("This is the arg: " + args);
+        if (args["text"] == " ") {
+            type(pressedKeysList.reduce((x,y) => x + y) + " ");
+
+            pressedKeysList = [];
+        } else {
+            pressedKeysList.push(args["text"]);
+        }
         
 	});
 }
